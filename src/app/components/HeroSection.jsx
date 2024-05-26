a57e1a81a659423448c6eeaaece0799777280ac2
KAVYA SINGH <97382504+kavya343@users.noreply.github.com>
2024-05-26 18:01:02 +0530

    done

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,73 +1,80 @@
-'use client';
-import React from 'react'
-import Image from 'next/image';
-import { TypeAnimation } from 'react-type-animation';
+"use client";
+import React from "react";
+import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
-
-
-
+import Link from "next/link";
 
 const HeroSection = () => {
   return (
-    <section className='lg:py-16'>
-        <div className='grid grid-cols-1 sm:grid-cols-12'>
+    <section className="lg:py-16">
+      <div className="grid grid-cols-1 sm:grid-cols-12">
         <motion.div
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
-          className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+          className="col-span-7 place-self-center text-center sm:text-left justify-self-start"
         >
-        <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl font-extrabold">
-            <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-300 to-pink-500'>Hello, I'm </span>
-            <br />
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-7xl lg:leading-normal font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
+              Hello, I&apos;m{" "}
+            </span>
+            <br></br>
             <TypeAnimation
-      sequence={[
-        // Same substring at the start will only be typed out once, initially
-        'Kavya',
-        1000, // wait 1s before replacing "Mice" with "Hamsters"
-        'Web Developer',
-        1000,
-        'Front-End Developer',
-        1000,
-        'Full Stack Developer',
-        1000
-      ]}
-      wrapper="span"
-      speed={50}
-      repeat={Infinity}
-    />
-            
-            </h1>
-        <p className='text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl'>
-            Welcome to my portfolio! I'm a passionate web developer with a 
-            strong background in creating dynamic and responsive 
-            websites and applications. 
-            </p>
-            <div>
-                <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-400 via-purple-400 to-pink-400 hover:bg-slate-200 text-white'>Hire Me</button>
-                <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-400 via-purple-400 to-pink-400 hover:bg-slate-800 text-white  mt-3'><span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Download CV</span></button>
-            </div>
-            </motion.div>
-            <motion.div
+              sequence={[
+                "Kavya Singh",
+                1000,
+                "Web Developer",
+                1000,
+                "Front-End Developer",
+                1000,
+                "Full Stack Developer",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+          </h1>
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+          Welcome to my portfolio! I'm a passionate web developer with a strong background in creating dynamic and responsive websites and applications.
+          </p>
+          <div>
+            <Link
+              href="/#contact"
+              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
+            >
+              Hire Me
+            </Link>
+            <Link
+              href="https://docs.google.com/document/d/1WXrjVCmoJOqRiSvk0ca8eQuJiofZsvDD/edit?usp=sharing&ouid=112924988295368272934&rtpof=true&sd=true "
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+            >
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download CV
+              </span>
+            </Link>
+          </div>
+        </motion.div>
+        <motion.div
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-
-                <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:r-[400px] relative">
-              <Image 
-              src='/images/hero-image.jpg'
-              alt='hero image'
-              className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
-              width={250}
-              height={250}
-              />
-              </div>
-            </motion.div>
-        </div>
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src="/images/hero-image.jpg"
+              alt="hero image"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={300}
+              height={300}
+            />
+          </div>
+        </motion.div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
