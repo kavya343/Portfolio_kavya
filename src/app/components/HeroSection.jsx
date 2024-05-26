38ce85f383be4f913bc420795f174ddccbdd848a
KAVYA SINGH <97382504+kavya343@users.noreply.github.com>
2024-05-26 17:12:49 +0530

    by kavya

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import Image from 'next/image';
 import { TypeAnimation } from 'react-type-animation';
+import { motion } from "framer-motion";
 
 
 
@@ -10,7 +11,12 @@ const HeroSection = () => {
   return (
     <section className='lg:py-16'>
         <div className='grid grid-cols-1 sm:grid-cols-12'>
-            <div className='col-span-7 place-self-center text-center sm:text-left'>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+        >
         <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl font-extrabold">
             <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-300 to-pink-500'>Hello, I'm </span>
             <br />
@@ -41,8 +47,14 @@ const HeroSection = () => {
                 <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-400 via-purple-400 to-pink-400 hover:bg-slate-200 text-white'>Hire Me</button>
                 <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-400 via-purple-400 to-pink-400 hover:bg-slate-800 text-white  mt-3'><span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Download CV</span></button>
             </div>
-            </div>
-            <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+            </motion.div>
+            <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-4 place-self-center mt-4 lg:mt-0"
+        >
+
                 <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:r-[400px] relative">
               <Image 
               src='/images/hero-image.jpg'
@@ -52,7 +64,7 @@ const HeroSection = () => {
               height={250}
               />
               </div>
-            </div>
+            </motion.div>
         </div>
     </section>
   )
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,10 @@ import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import AboutSection from "./components/AboutSection";
+import ProjectsSection from "./components/ProjectsSection";
+import AchievementsSection from "./components/AchievementsSection";
+import EmailSection from "./components/EmailSection";
+import Footer from "./components/Footer";
 
 
 export default function Home() {
@@ -10,7 +14,11 @@ export default function Home() {
           <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4"> 
       <HeroSection />
+      <AchievementsSection />
       <AboutSection />
+      <ProjectsSection />
+      <EmailSection />
+      <Footer />
       </div>
     </main>
   );
